89121ef402daefed2e8a67f38bc48120a046f923
davidb <davidb@bit-numbers.com>
2016-09-08 16:12:20 +0800

    latest update of the day

--- DeCompactPlugIn/DeCompactPlugInCommandHandler.cs
+++ DeCompactPlugIn/DeCompactPlugInCommandHandler.cs
@@ -5,6 +5,8 @@ using System.Text;
 
 using Slb.Ocean.Petrel.Commands;
 using Slb.Ocean.Petrel;
+using System.Windows.Forms;
+using Slb.Ocean.Petrel.Contexts;
 
 namespace DeCompactPlugIn
 {
@@ -12,19 +14,31 @@ namespace DeCompactPlugIn
     {
         public static string ID = "DeCompactPlugIn.DeCompactPlugInCommand";
 
-        #region SimpleCommandHandler Members
-
-        public override bool CanExecute(Slb.Ocean.Petrel.Contexts.Context context)
-        { 
+        private Form _form;
+        /// <summary>
+        /// Always enabled.
+        /// </summary>
+        /// <param name="context">system context</param>
+        /// <returns>True to keep it always enabled</returns>
+        public override bool CanExecute(Context context)
+        {
             return true;
         }
+        /// <summary>
+        /// Shows the dialog to copy the grid and create properties containing data only along the faults,
+        /// derived from original proeprties. 
+        /// </summary>
+        /// <param name="context">System context</param>
+        public override void Execute(Context context)
+        {
+            // create form if not already created or disposed
+            if (_form == null || _form.IsDisposed)
+                //_form = new DeCompactWorkStepUI();
 
-        public override void Execute(Slb.Ocean.Petrel.Contexts.Context context)
-        {          
-            //TODO: Add command execution logic here
-            PetrelLogger.InfoOutputWindow(string.Format("{0} clicked", @"DeCompactPlugIn" ));
+            if (!_form.Visible)
+                PetrelSystem.ShowModeless(_form);
+            else
+                _form.Hide();
         }
-    
-        #endregion
     }
 }
